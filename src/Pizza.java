2ff2b6d8fdc1efc5112d7a58bf53e8cda7405c18
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2020-10-29 15:25:45 +0100

    Added get and set

--- src/Pizza.java
+++ src/Pizza.java
@@ -4,6 +4,14 @@ String topping;
     public Pizza(String topping) {
         this.topping = topping;
     }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
 }
 
 
